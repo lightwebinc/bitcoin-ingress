6eab25f95110573ea93ad14d89751ca72ff5b632
Jeff Harris <26550587+jefflightweb@users.noreply.github.com>
2026-04-15 11:55:17 -0600

    [Fix] local vars, alignment

--- terraform/examples/generic/main.tf
+++ terraform/examples/generic/main.tf
@@ -16,17 +16,17 @@ terraform {
 module "bgp" {
   source = "../../modules/bgp-anycast"
 
-  enable_bgp      = var.enable_bgp
-  bgp_daemon      = var.bgp_daemon
-  bgp_prefix      = var.bgp_prefix
-  bgp_vip         = var.bgp_vip
-  bgp_prefix6     = var.bgp_prefix6
-  bgp_vip6        = var.bgp_vip6
-  bgp_local_as    = var.bgp_local_as
-  bgp_peer_as     = var.bgp_peer_as
-  bgp_peer_ip     = var.bgp_peer_ip
-  bgp_peer_ip6    = var.bgp_peer_ip6
-  bgp_password    = var.bgp_password
+  enable_bgp   = var.enable_bgp
+  bgp_daemon   = var.bgp_daemon
+  bgp_prefix   = var.bgp_prefix
+  bgp_vip      = var.bgp_vip
+  bgp_prefix6  = var.bgp_prefix6
+  bgp_vip6     = var.bgp_vip6
+  bgp_local_as = var.bgp_local_as
+  bgp_peer_as  = var.bgp_peer_as
+  bgp_peer_ip  = var.bgp_peer_ip
+  bgp_peer_ip6 = var.bgp_peer_ip6
+  bgp_password = var.bgp_password
 }
 
 # Provision each host via Ansible
--- terraform/modules/bgp-anycast/main.tf
+++ terraform/modules/bgp-anycast/main.tf
@@ -8,19 +8,19 @@ terraform {
 
 locals {
   bgp_vars = {
-    enable_bgp      = tostring(var.enable_bgp)
-    bgp_daemon      = var.bgp_daemon
-    bgp_prefix      = var.bgp_prefix
-    bgp_vip         = var.bgp_vip
-    bgp_prefix6     = var.bgp_prefix6
-    bgp_vip6        = var.bgp_vip6
-    bgp_local_as    = tostring(var.bgp_local_as)
-    bgp_peer_as     = tostring(var.bgp_peer_as)
-    bgp_peer_ip     = var.bgp_peer_ip
-    bgp_peer_ip6    = var.bgp_peer_ip6
-    bgp_router_id   = var.bgp_router_id
-    bgp_hold_time   = tostring(var.bgp_hold_time)
-    bgp_keepalive   = tostring(var.bgp_keepalive)
-    bgp_password    = var.bgp_password
+    enable_bgp    = tostring(var.enable_bgp)
+    bgp_daemon    = var.bgp_daemon
+    bgp_prefix    = var.bgp_prefix
+    bgp_vip       = var.bgp_vip
+    bgp_prefix6   = var.bgp_prefix6
+    bgp_vip6      = var.bgp_vip6
+    bgp_local_as  = tostring(var.bgp_local_as)
+    bgp_peer_as   = tostring(var.bgp_peer_as)
+    bgp_peer_ip   = var.bgp_peer_ip
+    bgp_peer_ip6  = var.bgp_peer_ip6
+    bgp_router_id = var.bgp_router_id
+    bgp_hold_time = tostring(var.bgp_hold_time)
+    bgp_keepalive = tostring(var.bgp_keepalive)
+    bgp_password  = var.bgp_password
   }
 }
